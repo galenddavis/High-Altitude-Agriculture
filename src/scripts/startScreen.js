fec5e88f480464300b4668dbe5efa282a25a45a4
trash <73361653+galenddavis@users.noreply.github.com>
2021-03-10 16:39:47 -0500

    Sprite animation working, Start screen switches

--- src/scripts/startScreen.js
+++ src/scripts/startScreen.js
@@ -2,8 +2,8 @@
 class StartScreen {
     constructor(ctx) {
         this.ctx = ctx;
-        // this.background = new Image();
-        // this.background.src = '/src/assets/images/game_start_background.png'
+        this.background = new Image();
+        this.background.src = '/src/assets/images/game_start_background.png'
         this.ship = new Image();
         this.ship.src = '/src/assets/images/spaceship_sprite_sheet.png'
         // this.ship.src = '/src/assets/images/spaceship.gif'
@@ -13,31 +13,36 @@ class StartScreen {
         this.y = 0;
         this.srcX = 0;
         this.srcY = 0;
-        this.sheetWidth = 3157;
+        this.sheetWidth = 7749;
         this.sheetHeight = 202;
-        this.frameCount = 10
+        this.frameCount = 27
+        // this.width = 1000
+        // this.height = 1000
         this.width = this.sheetWidth / this.frameCount
         this.height = this.sheetHeight
         this.currentFrame = 0;
+        this.frameCounter = 0
     }
 
     
     update() {
-        // let currentFrame = 0
-        this.currentFrame = ++this.currentFrame % this.frameCount;
-        this.srcX = this.currentFrame * this.width
-        this.srcY = 0
-        
-        
+        this.frameCounter += 1
+        if (this.frameCounter === 8) {
+            this.currentFrame = ++this.currentFrame % this.frameCount;
+            this.srcX = this.currentFrame * this.width
+            this.srcY = 0
+            this.frameCounter = 0
+        }
     }
 
-    draw(ctx) {
+    draw(ctx1) {
         this.update();
-        // ctx.drawImage(this.ship, this.srcX, this.srcY, this.width, this.height)
-        ctx.drawImage(this.ship, 0,0)
-        ctx.font = "50px DotGothic16";
-        ctx.fillStyle = 'white'
-        ctx.fillText("Click Anywhere to Start", 440, 650);
+        ctx1.drawImage(this.background, 0, 0)
+        ctx1.drawImage(this.ship, this.srcX, this.srcY, this.width, this.height, 600, 250, 287, 202)
+
+        ctx1.font = "50px DotGothic16";
+        ctx1.fillStyle = 'white'
+        ctx1.fillText("Click Anywhere to Start", 440, 650);
     }
 }
 
